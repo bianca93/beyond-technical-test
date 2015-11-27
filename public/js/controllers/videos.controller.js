143413282397cfaab3844d2f8245e77b2c9a8fb4
bianca93 <bianca.trif28@gmail.com>
2015-11-27 23:27:45 +0200

    Added stub for search playlist function

--- public/js/controllers/videos.controller.js
+++ public/js/controllers/videos.controller.js
@@ -3,5 +3,10 @@
 angular.module('youtubeVideos').controller('VideosController', ['$scope',
  function($scope) {
 	var vm = this;
+  vm.playlist = {};
+
+  vm.searchPlaylist = function() {
+    // search for playlist
+  };
 
 }]);
