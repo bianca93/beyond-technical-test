02618310d23790d7bb897ae675e74bf756ee843f
bianca93 <bianca.trif28@gmail.com>
2015-11-28 02:26:18 +0200

    Added an extra query param for the api request to specify the type

--- public/js/services/youtubeSearch.service.js
+++ public/js/services/youtubeSearch.service.js
@@ -2,15 +2,17 @@
 
 angular.module('youtubeVideos').factory('youtubeSearch', ['$resource', 
   function($resource) {
-    return $resource('https://www.googleapis.com/youtube/v3/search?part=:part&q=:query&key=:apiKey', {
-      query: '@query'
+    return $resource('https://www.googleapis.com/youtube/v3/search?part=:part&q=:query&type=:type&key=:apiKey', {
+      query: '@query',
+      type: '@type'
     },{
       search: {
         method: 'GET',
         params: {
           part: 'snippet',
           query: '@query',
-          apiKey: '<INSERT API KEY HERE>'
+          type: '@type',
+          apiKey: 'INSERT API KEY HERE'
         }
       }
     });
