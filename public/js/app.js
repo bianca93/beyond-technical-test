9a6c61cd43949d61c89794bd8046f0c165006918
bianca93 <bianca.trif28@gmail.com>
2015-11-29 14:23:38 +0200

    Added dependecy for ngMessages

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-var app = angular.module('youtubeVideos', ['ui.router', 'ngAnimate', 'ngResource', 'ngMaterial']);
+var app = angular.module('youtubeVideos', ['ui.router', 'ngAnimate', 'ngResource', 'ngMaterial', 'ngMessages']);
