2c0d851e21cda9a01d1b0d7681155f2c78c4bc1a
bianca93 <bianca.trif28@gmail.com>
2015-11-29 17:31:48 +0200

    Added emit event on clicked element

--- public/js/directives/searchItem.directive.js
+++ public/js/directives/searchItem.directive.js
@@ -3,6 +3,15 @@
 angular.module('youtubeVideos').directive('searchItem', function() {
   return {
     restrict: 'E',
-    templateUrl: '/views/partials/searchItem.html'
+    scope: {
+      element: '='
+    },
+    replace: true,
+    templateUrl: '/views/partials/searchItem.html',
+    link: function($scope, $element) {
+      $element.bind('click', function(){
+        $scope.$emit('selected', $scope.element.id.playlistId);
+      });
+    }
   };
 });
