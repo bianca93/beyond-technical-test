98960b03db77dd3cf879b0f727b04c8e91a91745
bianca93 <bianca.trif28@gmail.com>
2015-11-30 04:35:42 +0200

    Emitted event when video is selected too

--- public/js/directives/searchItem.directive.js
+++ public/js/directives/searchItem.directive.js
@@ -4,21 +4,30 @@ angular.module('youtubeVideos').directive('searchItem', function() {
   return {
     restrict: 'E',
     scope: {
-      element: '='
+      element: '=',
+      type: '='
     },
     replace: true,
     templateUrl: '/views/partials/searchItem.html',
-    link: function($scope, $element) {
+    link: function($scope, $element, $window) {
+      $scope.thumbnail = {};
+
+      if($window.innerWidth <= 480) {
+        $scope.thumbnail.url = $scope.element.snippet.thumbnails.default.url;
+      } else {
+        $scope.thumbnail.url = $scope.element.snippet.thumbnails.medium.url;
+      }
+
       $element.bind('click', function(){
-        id($scope.element = playlist) {
+        if($scope.type === 'playlist') {
           $scope.$emit('selected', { 
             data: $scope.element.id.playlistId, 
-            type: 'playlist'
+            type: $scope.type
           });
-        } else {
+        } else if($scope.type === 'video') {
           $scope.$emit('selected', { 
-            data: $scope.element.id.videoId, 
-            type: 'video'
+            data: $scope.element.snippet.resourceId.videoId, 
+            type: $scope.type
           });
         }
       });
